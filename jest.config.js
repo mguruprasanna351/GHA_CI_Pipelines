abe9619b2ccd828389e19755a3287e6083b362bf
Jittu Tech <mguruprasanna7@gmail.com>
2024-12-29 09:48:59 +0530

    updated the latest veriosn of ts and dependecies file

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,12 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  testMatch: ['**/*.test.ts'],
+  transform: {
+    '^.+\\.ts$': 'ts-jest'
+  },
+  moduleFileExtensions: ['ts', 'js', 'json'],
   coverageDirectory: 'coverage',
   collectCoverageFrom: ['src/**/*.ts'],
   coverageThreshold: {
