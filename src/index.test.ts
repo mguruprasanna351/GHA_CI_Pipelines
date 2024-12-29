df385bf6566a9a306de5be829b6a16e05eb56371
Jittu Tech <mguruprasanna7@gmail.com>
2024-12-29 08:45:34 +0530

    files have been updated

---
+++ src/index.test.ts
@@ -0,0 +1,16 @@
+import request from 'supertest';
+import express from 'express';
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.json({ message: 'Hello World!' });
+});
+
+describe('API Tests', () => {
+  it('should return hello world message', async () => {
+    const response = await request(app).get('/');
+    expect(response.status).toBe(200);
+    expect(response.body).toEqual({ message: 'Hello World!' });
+  });
+});
