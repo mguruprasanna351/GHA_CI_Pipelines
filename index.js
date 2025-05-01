0f4464d88c9589f146fffad0a4ab8f0f63c085f1
Jittu Tech <mguruprasanna7@gmail.com>
2025-05-01 19:28:33 +0530

    updated

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 // run `node index.js` in the terminal
-
+// This is a simple Node.js script that prints the current version of Node.js to the console.
 console.log(`Hello Node.js v${process.versions.node}!`);
